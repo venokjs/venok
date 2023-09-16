3a8feb460014f857b1d29f1781cd9a3914e082df
0x303133 <0x303133@proton.me>
2023-09-16 21:36:07 +0300

    feat(core): Create compiler for module

---
+++ packages/core/injector/compiler.ts
@@ -0,0 +1,37 @@
+import { Type } from "@venok/core/interfaces";
+import { DynamicModule } from "@venok/core/interfaces/modules/dynamic-module.interface";
+import { ForwardReference } from "@venok/core/interfaces/modules/forward-reference.interface";
+import { ModuleTokenFactory } from "@venok/core/injector/module-token-factory";
+
+export interface ModuleFactory {
+  type: Type<any>;
+  token: string;
+  dynamicMetadata?: Partial<DynamicModule>;
+}
+
+export class ModuleCompiler {
+  constructor(private readonly moduleTokenFactory = new ModuleTokenFactory()) {}
+
+  public async compile(metatype: Type<any> | DynamicModule | Promise<DynamicModule>): Promise<ModuleFactory> {
+    const { type, dynamicMetadata } = this.extractMetadata(await metatype);
+    const token = this.moduleTokenFactory.create(type, dynamicMetadata);
+    return { type, dynamicMetadata, token };
+  }
+
+  public extractMetadata(metatype: Type<any> | ForwardReference | DynamicModule): {
+    type: Type<any>;
+    dynamicMetadata?: Partial<DynamicModule> | undefined;
+  } {
+    if (!this.isDynamicModule(metatype)) {
+      return {
+        type: (metatype as ForwardReference)?.forwardRef ? (metatype as ForwardReference).forwardRef() : metatype,
+      };
+    }
+    const { module: type, ...dynamicMetadata } = metatype;
+    return { type, dynamicMetadata };
+  }
+
+  public isDynamicModule(module: Type<any> | DynamicModule | ForwardReference): module is DynamicModule {
+    return !!(module as DynamicModule).module;
+  }
+}
