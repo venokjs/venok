89a9e8d89874e3175bbb2ca112fe9d694a9fbf40
0x303133 <0x303133@proton.me>
2023-09-19 08:34:40 +0300

    feat(core): Create Discovery

---
+++ packages/core/discovery/discovery-module.ts
@@ -0,0 +1,12 @@
+import { MetadataScanner } from "@venok/core/metadata-scanner";
+import { Module } from "@venok/core/decorators/module.decorator";
+import { DiscoveryService } from "@venok/core/discovery/discovery-service";
+
+/**
+ * @publicApi
+ */
+@Module({
+  providers: [MetadataScanner, DiscoveryService],
+  exports: [MetadataScanner, DiscoveryService],
+})
+export class DiscoveryModule {}
