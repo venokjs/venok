7b31e33dddbff5fcf81e3b4f547cd08bb976135b
0x303133 <0x303133@proton.me>
2023-09-22 19:17:31 +0300

    feat(core): Create guards instances

---
+++ packages/core/interfaces/features/guards.interface.ts
@@ -0,0 +1,21 @@
+import { Observable } from "rxjs";
+import { ExecutionContext } from "@venok/core/interfaces/context/execution.interface";
+
+/**
+ * Interface defining the `canActivate()` function that must be implemented
+ * by a guard.  Return value indicates whether the current request is
+ * allowed to proceed.  Return can be either synchronous (`boolean`)
+ * or asynchronous (`Promise` or `Observable`).
+ *
+ * @publicApi
+ */
+export interface CanActivate {
+  /**
+   * @param context Current execution context. Provides access to details about
+   * the current request pipeline.
+   *
+   * @returns Value indicating whether the current request is allowed to
+   * proceed.
+   */
+  canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean>;
+}
