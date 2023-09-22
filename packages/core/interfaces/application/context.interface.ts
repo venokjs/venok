c4044dfcf49f01bc2d8aa70beae8bc7a951753be
0x303133 <0x303133@proton.me>
2023-09-22 19:07:52 +0300

    feat(core): Create app and global context

--- packages/core/interfaces/application/context.interface.ts
+++ packages/core/interfaces/application/context.interface.ts
@@ -1,3 +1,7 @@
+import { DynamicModule, Type } from "@venok/core/interfaces";
+import { LoggerService, LogLevel } from "@venok/core/services/logger.service";
+import { ShutdownSignal } from "@venok/core/application/context";
+
 export interface GetOrResolveOptions {
   /**
    * If enabled, lookup will only be performed in the host module.
@@ -10,4 +14,131 @@ export interface GetOrResolveOptions {
    * @default false
    */
   each?: boolean;
+}
+
+/**
+ * Interface defining ApplicationContext.
+ *
+ * @publicApi
+ */
+export interface VenokApplicationContext {
+  /**
+   * Allows navigating through the modules tree, for example, to pull out a specific instance from the selected module.
+   * @returns {VenokApplicationContext}
+   */
+  select<T>(module: Type<T> | DynamicModule): VenokApplicationContext;
+
+  /**
+   * Retrieves an instance of either injectable or controller, otherwise, throws exception.
+   * @returns {TResult}
+   */
+  get<TInput = any, TResult = TInput>(typeOrToken: Type<TInput> | Function | string | symbol): TResult;
+  /**
+   * Retrieves an instance of either injectable or controller, otherwise, throws exception.
+   * @returns {TResult}
+   */
+  get<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    options: { strict?: boolean; each?: undefined | false },
+  ): TResult;
+  /**
+   * Retrieves a list of instances of either injectables or controllers, otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  get<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    options: { strict?: boolean; each: true },
+  ): Array<TResult>;
+  /**
+   * Retrieves an instance (or a list of instances) of either injectable or controller, otherwise, throws exception.
+   * @returns {TResult | Array<TResult>}
+   */
+  get<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    options?: GetOrResolveOptions,
+  ): TResult | Array<TResult>;
+
+  /**
+   * Resolves transient or request-scoped instance of either injectable or controller, otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  resolve<TInput = any, TResult = TInput>(typeOrToken: Type<TInput> | Function | string | symbol): Promise<TResult>;
+  /**
+   * Resolves transient or request-scoped instance of either injectable or controller, otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  resolve<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    contextId?: { id: number },
+  ): Promise<TResult>;
+  /**
+   * Resolves transient or request-scoped instance of either injectable or controller, otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  resolve<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    contextId?: { id: number },
+    options?: { strict?: boolean; each?: undefined | false },
+  ): Promise<TResult>;
+  /**
+   * Resolves transient or request-scoped instances of either injectables or controllers, otherwise, throws exception.
+   * @returns {Array<TResult>}
+   */
+  resolve<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    contextId?: { id: number },
+    options?: { strict?: boolean; each: true },
+  ): Promise<Array<TResult>>;
+  /**
+   * Resolves transient or request-scoped instance (or a list of instances) of either injectable or controller, otherwise, throws exception.
+   * @returns {Promise<TResult | Array<TResult>>}
+   */
+  resolve<TInput = any, TResult = TInput>(
+    typeOrToken: Type<TInput> | Function | string | symbol,
+    contextId?: { id: number },
+    options?: GetOrResolveOptions,
+  ): Promise<TResult | Array<TResult>>;
+
+  /**
+   * Registers the request/context object for a given context ID (DI container sub-tree).
+   * @returns {void}
+   */
+  registerRequestByContextId<T = any>(request: T, contextId: { id: number }): void;
+
+  /**
+   * Terminates the application
+   * @returns {Promise<void>}
+   */
+  close(): Promise<void>;
+
+  /**
+   * Sets custom logger service.
+   * Flushes buffered logs if auto flush is on.
+   * @returns {void}
+   */
+  useLogger(logger: LoggerService | LogLevel[] | false): void;
+
+  /**
+   * Prints buffered logs and detaches buffer.
+   * @returns {void}
+   */
+  flushLogs(): void;
+
+  /**
+   * Enables the usage of shutdown hooks. Will call the
+   * `onApplicationShutdown` function of a provider if the
+   * process receives a shutdown signal.
+   *
+   * @returns {this} The Venok application context instance
+   */
+  enableShutdownHooks(signals?: ShutdownSignal[] | string[]): this;
+
+  /**
+   * Initializes the Venok application.
+   * Calls the Venok lifecycle events.
+   * It isn't mandatory to call this method directly.
+   *
+   * @returns {Promise<this>} The ApplicationContext instance as Promise
+   */
+  init(): Promise<this>;
 }
