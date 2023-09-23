5eb026de1d55ab3135d17d3720eaf39bb83318fa
0x303133 <0x303133@proton.me>
2023-09-23 19:41:06 +0300

    feat(core): Create configurable modules

---
+++ packages/core/interfaces/modules/configurable/cls.interface.ts
@@ -0,0 +1,26 @@
+import { DEFAULT_FACTORY_CLASS_METHOD_KEY, DEFAULT_METHOD_KEY } from "@venok/core/module/constants";
+import { DynamicModule } from "@venok/core/interfaces";
+import { ConfigurableModuleAsyncOptions } from "@venok/core/interfaces/modules/configurable/async-options.interface";
+
+/**
+ * Class that represents a blueprint/prototype for a configurable Nest module.
+ * This class provides static methods for constructing dynamic modules. Their names
+ * can be controlled through the "MethodKey" type argument.
+ *
+ * @publicApi
+ */
+export type ConfigurableModuleCls<
+  ModuleOptions,
+  MethodKey extends string = typeof DEFAULT_METHOD_KEY,
+  FactoryClassMethodKey extends string = typeof DEFAULT_FACTORY_CLASS_METHOD_KEY,
+  ExtraModuleDefinitionOptions = {},
+> = {
+  new (): any;
+} & Record<`${MethodKey}`, (options: ModuleOptions & Partial<ExtraModuleDefinitionOptions>) => DynamicModule> &
+  Record<
+    `${MethodKey}Async`,
+    (
+      options: ConfigurableModuleAsyncOptions<ModuleOptions, FactoryClassMethodKey> &
+        Partial<ExtraModuleDefinitionOptions>,
+    ) => DynamicModule
+  >;
