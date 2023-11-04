1f4c48e5d9f867507c37a863acad15c4a0b0d83e
0x303133 <0x303133@proton.me>
2023-11-04 23:10:53 +0300

    refactor(http): Move http from packages/http to network/http

---
+++ network/http/helpers/adapter.helper.ts
@@ -0,0 +1,36 @@
+import { AbstractHttpAdapter } from "../adapter/adapter";
+import { RequestMethod } from "../enums";
+import { VersioningOptions, VersionValue } from "../interfaces/router/version-options.interface";
+
+export class NoopHttpAdapter extends AbstractHttpAdapter {
+  constructor(instance: any) {
+    super(instance);
+  }
+  close(): any {}
+  initHttpServer(options: any): any {}
+  useStaticAssets(...args: any[]): any {}
+  setViewEngine(engine: string): any {}
+  getRequestHostname(request: any): any {}
+  getRequestMethod(request: any): any {}
+  getRequestUrl(request: any): any {}
+  reply(response: any, body: any): any {}
+  end(response: any, message?: any): any {}
+  status(response: any, statusCode: number): any {}
+  render(response: any, view: string, options: any): any {}
+  redirect(response: any, statusCode: number, url: string) {}
+  setErrorHandler(handler: Function, prefix = "/"): any {}
+  setNotFoundHandler(handler: Function, prefix = "/"): any {}
+  isHeadersSent(response: any): any {}
+  setHeader(response: any, name: string, value: string): any {}
+  registerParserMiddleware(): any {}
+  enableCors(options: any): any {}
+  createMiddlewareFactory(requestMethod: RequestMethod): any {}
+  getType() {
+    return "";
+  }
+  applyVersionFilter(handler: Function, version: VersionValue, versioningOptions: VersioningOptions) {
+    return (req: any, res: any, next: any) => {
+      return () => {};
+    };
+  }
+}
