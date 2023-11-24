406b1122d7b241c9239d52173a348482c366123f
0x303133 <0x303133@proton.me>
2023-11-24 22:09:20 +0300

    refactor(core): Move to integration

--- packages/core/helpers/messages.helper.ts
+++ packages/core/helpers/messages.helper.ts
@@ -1,4 +1,1 @@
 export const MODULE_INIT_MESSAGE = (text: TemplateStringsArray, module: string) => `${module} dependencies initialized`;
-
-export const INVALID_EXECUTION_CONTEXT = (methodName: string, currentContext: string) =>
-  `Calling ${methodName} is not allowed in this context. Your current execution context is "${currentContext}".`;
