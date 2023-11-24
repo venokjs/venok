1dde6968c45c94cbfeae5163c908994398c12a7d
0x303133 <0x303133@proton.me>
2023-11-24 22:14:23 +0300

    chore(core): Add index files for injector

--- packages/core/injector/index.ts
+++ packages/core/injector/index.ts
@@ -1,5 +1,12 @@
-export * from "./container";
-export { ContextId, HostComponentInfo } from "./instance/wrapper";
+export * from "./helpers";
+
+export * from "./instance/wrapper";
+
 export * from "./module/lazy/loader";
-export * from "./module/ref";
 export * from "./module/container";
+export * from "./module/module";
+export * from "./module/ref";
+
+export * from "./constants";
+export * from "./container";
+export * from "./injector";
