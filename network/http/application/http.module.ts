e2337182de6508e490b6460b1060c265c84d0527
0x303133 <0x303133@proton.me>
2024-01-01 20:54:52 +0300

    feat(http): Create HttpModule

---
+++ network/http/application/http.module.ts
@@ -0,0 +1,14 @@
+import { IntegrationModule } from "@venok/integration";
+import { Module } from "@venok/core";
+
+import { HttpConfig } from "@venok/http/application/config";
+import { HttpMiddlewareService } from "@venok/http/middleware";
+import { RoutesExplorer } from "@venok/http/explorers/routes.explorer";
+import { HttpStarterModule } from "@venok/http/application/starter.module";
+import { HttpConfigurableModuleClass } from "@venok/http/application/http.module-defenition";
+
+@Module({
+  imports: [IntegrationModule],
+  providers: [HttpConfig, HttpMiddlewareService, RoutesExplorer, HttpStarterModule],
+})
+export class HttpModule extends HttpConfigurableModuleClass {}
