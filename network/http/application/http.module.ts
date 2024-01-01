2dc6b0d08e57d99ec8c78dd54096a4ed0d5fd2c9
0x303133 <0x303133@proton.me>
2024-01-01 19:55:57 +0300

    feat(core): Delete old logic

--- network/http/application/http.module.ts
+++
@@ -1,9 +0,0 @@
-import { HttpConfigurableModuleClass } from "./http.module-defenition";
-import { Module } from "@venok/core";
-import { DiscoveryModule } from "@venok/core/discovery/module";
-import { HttpCoreService } from "./http.service";
-
-@Module({
-  providers: [DiscoveryModule, HttpCoreService],
-})
-export class HttpModule extends HttpConfigurableModuleClass {}
