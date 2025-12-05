bb29696031fc22b1c31c8a23e53e1f579d910e46
0x303133 <132019318+shiz-ceo@users.noreply.github.com>
2025-12-05 19:49:24 +0300

    feat(http): Add hostname & listenCallback to HttpAppOptions interface

--- packages/http/src/interfaces/options.interface.ts
+++ packages/http/src/interfaces/options.interface.ts
@@ -3,6 +3,8 @@ import type { HttpConfig } from "~/http/config.js";
 
 export interface HttpAppOptions {
   port: number;
+  hostname?: string;
+  listenCallback?: (...args: any[]) => Promise<void>;
   callback: (app: HttpConfig<any>) => void;
   adapter: AbstractHttpAdapter;
 }
