c59a35ef839d23ec1201933d28783edca9bfea15
0x303133 <132019318+shiz-ceo@users.noreply.github.com>
2025-12-29 21:04:47 +0300

    feat(websocket): Add constants & symbols

---
+++ packages/websocket/src/constants.ts
@@ -0,0 +1,8 @@
+import { ROUTE_ARGS_METADATA } from "@venok/core";
+
+export const MESSAGE_METADATA = "message";
+export const GATEWAY_SERVER_METADATA = "websockets:is_socket";
+export const GATEWAY_METADATA = "websockets:is_gateway";
+export const PORT_METADATA = "port";
+export const GATEWAY_OPTIONS = "websockets:gateway_options";
+export const PARAM_ARGS_METADATA = ROUTE_ARGS_METADATA;
