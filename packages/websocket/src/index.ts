738f0f31c86fda0b1b003953af20734faed60cc8
0x303133 <132019318+shiz-ceo@users.noreply.github.com>
2025-12-29 22:20:45 +0300

    chore(websocket): Create index file

--- packages/websocket/src/index.ts
+++ packages/websocket/src/index.ts
@@ -1,1 +1,23 @@
-export {};
+// eslint-disable-next-line custom/sort-imports
+import "reflect-metadata";
+
+export * from "./decorators/gateway.decorator.js";
+export * from "./decorators/message-body.decorator.js";
+export * from "./decorators/server.decorator.js";
+export * from "./decorators/socket.decorator.js";
+export * from "./decorators/subscribe-message.decorator.js";
+
+export * from "./exceptions/ws.exception.js";
+
+export * from "./helpers/event-stream-factory.helper.js";
+
+export * from "./interfaces/index.js";
+
+export * from "./websocket/adapter.js";
+export * from "./websocket/arguments-host.js";
+export * from "./websocket/config.js";
+export * from "./websocket/module.js";
+export * from "./websocket/params-factory.js";
+
+export * from "./constants.js";
+export * from "./symbols.js";
